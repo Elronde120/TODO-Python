247d63a134a3c913d0c2aa24aa04f68a9f15be2e
Elronde120 <sorelup@gmail.com>
2022-01-21 19:44:24 -0500

    Added main.py, added intro text

---
+++ main.py
@@ -0,0 +1,4 @@
+welcomeMessage = "Welcome to Basic ToDo [BTD]"
+menuCommandList = "Input a numbered command:\n[1] Create a ToDo list\n[2] Load a ToDo file\n"
+print(welcomeMessage)
+print(menuCommandList)
